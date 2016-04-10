e32d191abc6a8174be5b403fd3c2aaca4729c4af
Win10Lxp <Win10Lxp@ChuyunWin10Lxp>
2016-04-10 18:02:05 +0800

    测试团队开发

--- grails-app/controllers/cn/edu/cup/test/TestController.groovy
+++ grails-app/controllers/cn/edu/cup/test/TestController.groovy
@@ -6,6 +6,18 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class TestController {
 
+    @Transactional
+    def calculate(TestClazzA testClazzAInstance, String name) {
+
+        def b = new TestClazzB()
+        def test = new CalculatorA()
+        test.doCalculate(testClazzAInstance, b, name)
+        b.save(flush: true)
+        
+        chain(controller: "testClazzB", action: "index")
+            
+    }
+    
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
         respond TestClazzA.list(params), model:[testClazzAInstanceCount: TestClazzA.count()]
